72b7e21420154974d3f50108404d43c858fa26dd
Enio Demneri <enio1demneri@gmail.com>
2016-11-14 18:05:01 +0100

    project

--- index.js
+++ index.js
@@ -4,76 +4,57 @@ var shortid = require('shortid');
 var mongoose = require('mongoose');
 
 var app = new express();
+app.use(express.static('public'));
 
-mongoose.connect('mongodb://enio:enio@ds147267.mlab.com:47267/url', function(err){
-	if(err) throw err;
-	
-});
+var counter = 1;
+
+mongoose.connect('mongodb://enio:url@ds039421.mlab.com:39421/url');
 
 mongoose.connection.once('open', function(err){
-	if(err){
-		console.log(err);
-	}else{
+	if(err) console.log(err);
+	else { 
 		var snippetSchema = mongoose.Schema({
 			urls: {type:String},
 			shortUrl: {type:Number}		
 		});
 		
 		var Url = mongoose.model('Url', snippetSchema);
+		
+		app.get('/:url', function(req, res){
+			var url = 'http://' + req.params.url;
+			console.log(url);
+	
+			if(isNaN(Number(url))){
+				valid(url, function(err, valid){
+					if(err) throw err;
+					else console.log(valid);
+				});
+		
+				Url.create({urls: url, shortUrl: counter}, function(err, result){
+					if (err) console.log('Url does not exist');
+					else {
+						console.log('url created');
+						res.send(result.counter);
+					}
+				});
+				
+				counter += 1;
+		
+				Url.findOne({urls: url}, function(err, result){
+					if(err || !result) console.log(err);
+					else res.send(result.shortUrl);		
+				});	
+			}
+			else {
+	
+				Url.findOne({urls: url}, function(err, result){
+					if(err) console.log(err);
+					else if(result === null) res.send('Wrong id');
+					console.log(result);
+				});
+			}
+		});
 	}
 });
-
-app.use(express.static('public'));
-
-app.get('/:url', function(req, res){
-	var url = 'http://' + req.params.url;
-	console.log(url);
-	
-	if(!isNaN(Number(url))){
-		valid(url, function(err, valid){
-			if(err) throw err;
-			console.log(valid);
-		});
-			
 		
-		var id = Math.floor(Math.random() * 9999 + 1);
-		console.log(id);
-		
-		Url.create({urls: url, shortUrl: id}, function(err, result){
-			if(err){
-				console.log('Url does not exist');
-			}else{
-				console.log('url created');
-				res.json(id);
-			}
-		});
-		
-		Url.findOne({urls: url}, function(err, result){
-			if(err){
-				console.log(err);
-			}
-			
-			res.json(result.shortUrl);
-		});
-		
-
-		
-	}else{
-	
-		Url.findOne({urls: url}, function(err, result){
-			if(err){
-				console.log(err);
-			}
-			if(result === null){
-				res.json('Wrong id');
-			}
-			res.redirect(result.urls);
-		});
-	}
-	
-});
-		
-
-	
-
 app.listen(process.env.PORT || 7000);
